96ce25557e532b65003d7c388c72419069d504c4
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2023-01-27 02:49:59 +0300

    ADDED protocols and func for cell configuration based on fetched data types

--- ClvrtcTaskSeven/Controllers/Cells/FormFieldType.swift
+++ ClvrtcTaskSeven/Controllers/Cells/FormFieldType.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import UIKit
+
+
+protocol FieldUpdatable {
+    func update(with field: Field)
+}
+
+protocol FieldConformity {
+    var field: Field? { get set }
+}
+
+enum FormFieldType {
+    case textual
+    case numeric
+    case valuable
+    
+    static func registerCells(for tableView: UITableView) {
+        tableView.register(FormTextualTableViewCell.self, forCellReuseIdentifier: FormTextualTableViewCell.identifier)
+        tableView.register(FormNumericTableViewCell.self, forCellReuseIdentifier: FormNumericTableViewCell.identifier)
+        tableView.register(FormValuableTableViewCell.self, forCellReuseIdentifier: FormValuableTableViewCell.identifier)
+    }
+    
+    func dequeueCell(for tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell
+        
+        switch self {
+        case .textual:
+            cell = tableView.dequeueReusableCell(withIdentifier: FormTextualTableViewCell.identifier, for: indexPath)
+        case .numeric:
+            cell = tableView.dequeueReusableCell(withIdentifier: FormNumericTableViewCell.identifier, for: indexPath)
+        case .valuable:
+            cell = tableView.dequeueReusableCell(withIdentifier: FormValuableTableViewCell.identifier, for: indexPath)
+        }
+        
+        return cell
+    }
+}
